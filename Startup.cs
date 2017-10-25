2afb412f196708af31573efff741851421b775b6
SyrymJoli <syrym.joli@outlook.com>
2017-10-25 13:08:33 +0600

    code 8 Map

--- Startup.cs
+++ Startup.cs
@@ -18,20 +18,28 @@ namespace learnCore
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            //Выполнение app.Use
-            int x = 2;
-            app.Use(async (context, next) =>
-            {
-                x = x * 2;      // 2 * 2 = 4
-                await next.Invoke();    // вызов app.Run
-                x = x * 2;      // 8 * 2 = 16
-                await context.Response.WriteAsync($"Result: {x}");
-            });
+            // Метод Map
+            app.Map("/index", Index);
+            app.Map("/about", About);
         
             app.Run(async (context) =>
             {
-                x = x * 2;  //  4 * 2 = 8
-                await Task.FromResult(0);
+                await context.Response.WriteAsync("Page Not Found");
+            });
+        }
+
+        private static void Index(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("Index");
+            });
+        }
+        private static void About(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("About");
             });
         }
     }
