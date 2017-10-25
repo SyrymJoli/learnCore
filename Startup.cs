e4247f8249848f58d3d372a85650f7f19cab2ca3
SyrymJoli <syrym.joli@outlook.com>
2017-10-25 20:58:17 +0600

    code 17 OWIN

--- Startup.cs
+++ Startup.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.Extensions.FileProviders;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using System.Text;
+using System.IO;
 
 namespace learnCore
 {
@@ -18,13 +21,23 @@ namespace learnCore
 
         public void Configure(IApplicationBuilder app)
         {
-            app.UseDirectoryBrowser();
-            app.UseStaticFiles();
-            
-            app.Run(async (context) =>
+           app.UseOwin(pipeline =>
             {
-                await context.Response.WriteAsync("Hello World");
+                pipeline(next => SendResponseAsync);
             });
+        }
+
+        public Task SendResponseAsync(IDictionary<string, object> environment)
+        {
+            // получаем заголовки запроса
+            var requestHeaders = (IDictionary<string, string[]>)environment["owin.RequestHeaders"];
+            // получаем данные по User-Agent
+            string responseText = requestHeaders["User-Agent"][0];
+            byte[] responseBytes = Encoding.UTF8.GetBytes(responseText);
+            
+            var responseStream = (Stream)environment["owin.ResponseBody"];
+        
+            return responseStream.WriteAsync(responseBytes, 0, responseBytes.Length);
         }
     }
 }
