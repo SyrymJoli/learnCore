e10b825ebdeb129db33aab72510c4f43d9ee886e
SyrymJoli <syrym.joli@outlook.com>
2017-10-25 21:39:44 +0600

    code 18 logger

--- Startup.cs
+++ Startup.cs
@@ -1,14 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using Microsoft.Extensions.FileProviders;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using System.Text;
-using System.IO;
+using Microsoft.Extensions.Logging;
 
 namespace learnCore
 {
@@ -16,28 +13,20 @@ namespace learnCore
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDirectoryBrowser();
         }
 
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
         {
-           app.UseOwin(pipeline =>
-            {
-                pipeline(next => SendResponseAsync);
-            });
-        }
+           loggerFactory.AddConsole();
 
-        public Task SendResponseAsync(IDictionary<string, object> environment)
-        {
-            // получаем заголовки запроса
-            var requestHeaders = (IDictionary<string, string[]>)environment["owin.RequestHeaders"];
-            // получаем данные по User-Agent
-            string responseText = requestHeaders["User-Agent"][0];
-            byte[] responseBytes = Encoding.UTF8.GetBytes(responseText);
-            
-            var responseStream = (Stream)environment["owin.ResponseBody"];
-        
-            return responseStream.WriteAsync(responseBytes, 0, responseBytes.Length);
+           app.Run(async (context) => {
+               // создаем объект логгера
+               var logger = loggerFactory.CreateLogger("RequestInfoLogger");
+               // пишем на консоль информацию 
+               logger.LogInformation("Processing request: {0}", context.Request.Path);
+
+               await context.Response.WriteAsync("Hello World!");
+           });
         }
     }
 }
